8466678afe2915c0cff389899dd12238fe38a154
Dominik Fritsch <fritsch.dominik@outlook.de>
2024-10-10 13:40:47 +0200

    Add sub() function to main.cpp

--- FirstRepo/main.cpp
+++ FirstRepo/main.cpp
@@ -1,6 +1,7 @@
 #include "Warrior.h"
 
 int sum(int a, int b);
+int sub(int a, int b);
 
 int main()
 {
@@ -10,4 +11,9 @@ int main()
 int sum(x, y)
 {
 	return x + y;
+}
+
+int sub(x, y)
+{
+	return x - y;
 }
